eeb7c370427aec095f64e5df60ebbb61a93a39a9
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2020-10-18 00:24:02 +0200

    split Title, overview of watch time

--- scripts/View_history.R
+++ scripts/View_history.R
@@ -5,35 +5,45 @@ library(tidyverse)
 #################
 
 df <- read.csv(file = "data_files/ViewingActivity.csv",
-           header = T)
+           header = T) %>%
+  separate(col = Title,
+           into = c("Title", "Season", "episode"),
+           sep = ":") %>%
+filter(!Supplemental.Video.Type %in% 
+         c("HOOK","PREVIEW","RECAP","TRAILER","PROMOTIONAL","TEASER_TRAILER")) 
+
 
 # Parse the dates-and times columns in the df
 df$Start.Time <- parse_datetime(df$Start.Time)
 df$Duration <- parse_time(df$Duration)
 
+df <- df %>% filter(Duration > 60) # Filter very short "accidental" views 
+                                   #  Check by: df %>% ggplot(aes(x = Duration)) + geom_histogram(binwidth = 60)   
+
 ###################
 # Total watchtime #
 ###################
 
-df2 = df %>% mutate("day" = weekdays(Start.Time, abbreviate = TRUE),
+df <- df %>% mutate("day" = weekdays(Start.Time, abbreviate = TRUE),
                        "month" = months(Start.Time),
-                       "year" =  lubridate::year(Start.Time)
+                       "year" =  lubridate::year(Start.Time),
+                    "week" = lubridate::week(Start.Time)
                        ) 
-df2$day = factor(df2$day, levels = c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"),
+df$day = factor(df$day, levels = c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"),
                     ordered = TRUE)
-df2$month = factor(df2$month, levels = c("January","February","March","April",
+df$month = factor(df$month, levels = c("January","February","March","April",
                                          "May","June","July","August",
                                          "September","October","November","December"),
                    ordered = TRUE)
 
- df2 %>%
-  dplyr::group_by(day, month, year) %>%
-  dplyr::summarise("total" = sum(Duration)/3600) %>% 
-  ggplot(aes(x = day, y = total))+
-  geom_bar(stat = "identity")+
-  facet_grid(year~month, scale = "free")
+################################
+# Plot wacht history over time #
+################################
+
+ df %>%
+  ggplot(aes(x = lubridate::date(Start.Time), 
+             y = Duration))+
+  geom_bar(stat = "identity", aes(fill = month))+
+  facet_grid(~year, scale = "free")
  
- df2 %>%
-   ggplot(aes(x = lubridate::date(Start.Time),
-          y = Duration)) + 
-   geom_line()
+
